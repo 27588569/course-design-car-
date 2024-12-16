785e4a8707e44bc2652ba4342125ac415f03e872
吕翔峰 <2283876156@qq.com>
2024-12-16 14:09:26 +0800

    test: Pass move command test cases

--- src/Executorlmpl.cpp
+++ src/Executorlmpl.cpp
@@ -11,40 +11,40 @@ ExecutorImpl::ExecutorImpl(const Pose& pose) noexcept
 }
 void ExecutorImpl::Execute(const std::string& commands) noexcept  // 执行命令
 {
-    /* const char directions[] = {'N', 'E', 'S', 'W'};
-     const int numDirections = 4;
+    const char directions[] = {'N', 'E', 'S', 'W'};
+    const int numDirections = 4;
 
-     for (char ch : commands) {
-         switch (ch) {
-         case 'L':
-             // 左转
-             pose.heading = directions[(std::find(directions, directions + numDirections, pose.heading) - directions -
-                                        1 + numDirections) %
-                                       numDirections];
-             break;
-         case 'R':
-             // 右转
-             pose.heading =
-                 directions[(std::find(directions, directions + numDirections, pose.heading) - directions + 1) %
-                            numDirections];
-             break;
-         case 'M':
-             switch (pose.heading) {
-             case 'N':
-                 pose.y += 1;
-                 break;
-             case 'E':
-                 pose.x += 1;
-                 break;
-             case 'S':
-                 pose.y -= 1;
-                 break;
-             case 'W':
-                 pose.x -= 1;
-                 break;
-             }
-         }
-     }*/
+    for (char ch : commands) {
+        switch (ch) {
+        case 'L':
+            // 左转
+            pose.heading = directions[(std::find(directions, directions + numDirections, pose.heading) - directions -
+                                       1 + numDirections) %
+                                      numDirections];
+            break;
+        case 'R':
+            // 右转
+            pose.heading =
+                directions[(std::find(directions, directions + numDirections, pose.heading) - directions + 1) %
+                           numDirections];
+            break;
+        case 'M':
+            switch (pose.heading) {
+            case 'N':
+                pose.y += 1;
+                break;
+            case 'E':
+                pose.x += 1;
+                break;
+            case 'S':
+                pose.y -= 1;
+                break;
+            case 'W':
+                pose.x -= 1;
+                break;
+            }
+        }
+    }
 }
 Pose ExecutorImpl::Query() const noexcept
 {
