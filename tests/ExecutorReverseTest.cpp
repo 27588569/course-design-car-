d0e7705540f2c8c71cd32955f572b30eb34e55d0
吕翔峰 <2283876156@qq.com>
2024-12-18 22:57:24 +0800

    pass test cases for different car type

--- tests/ExecutorReverseTest.cpp
+++ tests/ExecutorReverseTest.cpp
@@ -12,9 +12,25 @@ TEST(ExecutorReverseTest, should_return_x_plus_1_given_carType_is_N_status_is_re
     ASSERT_EQ(target, executor->Query());
 }
 
+TEST(ExecutorReverseTest, should_return_x_plus_2_given_carType_is_S_status_is_reverse_command_is_M_and_facing_is_W)
+{
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'W',},'S'));
+    executor->Execute("BM");
+    const Pose target({2, 0, 'W'});
+    ASSERT_EQ(target, executor->Query());
+}
+
 TEST(ExecutorReverseTest, should_return_x_minus_1_given_carType_is_N_status_is_reverse_command_is_M_and_facing_is_E)
 {
     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'},'N'));
+    executor->Execute("BM");
+    const Pose target({-1, 0, 'E'});
+    ASSERT_EQ(target, executor->Query());
+}
+
+TEST(ExecutorReverseTest, should_return_x_minus_1_given_carType_is_B_status_is_reverse_command_is_M_and_facing_is_E)
+{
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'},'B'));
     executor->Execute("BM");
     const Pose target({-1, 0, 'E'});
     ASSERT_EQ(target, executor->Query());
